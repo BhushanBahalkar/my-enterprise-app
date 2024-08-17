e1a8fb130bb6d6f2e1ddcc5a1bf3ad6652312216
Bhushan Bahalkar <bhushanbahalkar189@gmail.com>
2024-08-17 18:16:02 +0530

    update src/index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
 // Create a root element using React 18's createRoot method
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 // Render the App component within the root element
 root.render(
