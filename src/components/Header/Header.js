4a8c3beced638b1d01a105d299316e6e404c79ee
Bhushan Bahalkar <bhushanbahalkar189@gmail.com>
2024-08-17 18:19:58 +0530

    update Header.css and Header.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,6 @@
 // Header.js
-import React, { useEffect, useState } from 'react';
-import './Header.css';
+import React, { useEffect, useState } from "react";
+import "./Header.css";
 
 const Header = () => {
   const [currentTime, setCurrentTime] = useState(new Date());
@@ -14,20 +14,26 @@ const Header = () => {
   }, []);
 
   const formatTime = (date) => {
-    const options = { hour: '2-digit', minute: '2-digit', second: '2-digit', hour12: true };
-    return date.toLocaleTimeString('en-US', options);
+    const options = {
+      hour: "2-digit",
+      minute: "2-digit",
+      second: "2-digit",
+      hour12: true,
+    };
+    return date.toLocaleTimeString("en-US", options);
   };
 
   const formatDate = (date) => {
-    const options = { year: 'numeric', month: 'long', day: '2-digit' };
-    return date.toLocaleDateString('en-US', options);
+    const options = { year: "numeric", month: "long", day: "2-digit" };
+    return date.toLocaleDateString("en-US", options);
   };
 
   return (
     <div className="header">
-      
       <div className="header-info">
-        <span>{formatDate(currentTime)} {formatTime(currentTime)}</span>
+        <span>
+          {formatDate(currentTime)} {formatTime(currentTime)}
+        </span>
         <span>Om Enterprises</span>
         <span>Good Morning! Mr. ABC</span>
       </div>
