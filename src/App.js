5c043e57f60002c485c63c520ded0e8be050c057
Bhushan Bahalkar <bhushanbahalkar189@gmail.com>
2024-08-17 18:11:52 +0530

    update App.js and App.css update App.css

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import React, { useState } from "react";
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
-import Dashboard from "./components/Dashboard/Dashboard";
-import EmpTable from "./components/EmployeeTable/EmpTable";
-import Navbar from "./components/Navbar/Navbar";
-import RoutesTable from "./components/Routes/RoutesTable";
-import Sidebar from "./components/Sidebar/Sidebar";
-import Vehicle from "./components/Vehicle/Vehicle";
-import Report from "./components/Reports/Report";
-import Login from "./components/LoginForm/LoginForm";
-import "./styles/App.css";
+import React, { useState } from 'react';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import Dashboard from './components/Dashboard/Dashboard'; 
+import EmpTable from './components/EmployeeTable/EmpTable';
+import Navbar from './components/Navbar/Navbar';
+import RoutesTable from './components/Routes/RoutesTable';
+import Sidebar from './components/Sidebar/Sidebar';
+import Vehicle from './components/Vehicle/Vehicle';
+import Report from './components/Reports/Report';
+import Login from './components/LoginForm/LoginForm';
+import './styles/App.css';
 
 function App() {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
@@ -20,22 +20,19 @@ function App() {
 
   return (
     <Router>
-      <div className={`app ${isSidebarOpen ? "sidebar-open" : ""}`}>
+      <div className={`app ${isSidebarOpen ? 'sidebar-open' : ''}`}>
         {isLoggedIn ? (
           <>
             <Sidebar isOpen={isSidebarOpen} />
             <div className="main-content">
-              <Navbar
-                toggleSidebar={toggleSidebar}
-                onLogout={() => setIsLoggedIn(false)}
-              />
+              <Navbar toggleSidebar={toggleSidebar} onLogout={() => setIsLoggedIn(false)} />
               <div className="content">
                 <Routes>
                   <Route path="/" element={<Dashboard />} />
                   <Route path="/employees" element={<EmpTable />} />
                   <Route path="/a" element={<Vehicle />} />
-                  <Route path="/routes" element={<RoutesTable />} />
-                  <Route path="/reports" element={<Report />} />
+                  <Route path="/routes" element={<RoutesTable/>}/>
+                  <Route path="/reports" element={<Report/>}/>
                 </Routes>
               </div>
             </div>
