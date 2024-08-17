b014f762494f0bf19eb84294096894af94381de5
Bhushan Bahalkar <bhushanbahalkar189@gmail.com>
2024-08-17 18:21:19 +0530

    update Navbar.css and Navbar.js

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faExpand, faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
-import './Navbar.css';
-import logo from '../logo/logoom.png';
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faExpand, faSignOutAlt } from "@fortawesome/free-solid-svg-icons";
+import "./Navbar.css";
+import logo from "../logo/logoom.png";
 
 const Navbar = ({ toggleSidebar, onLogout }) => {
   const [currentTime, setCurrentTime] = React.useState(new Date());
@@ -16,19 +16,26 @@ const Navbar = ({ toggleSidebar, onLogout }) => {
   }, []);
 
   const formatTime = (date) => {
-    const options = { hour: '2-digit', minute: '2-digit', second: '2-digit', hour12: true };
-    return date.toLocaleTimeString('en-US', options);
+    const options = {
+      hour: "2-digit",
+      minute: "2-digit",
+      second: "2-digit",
+      hour12: true,
+    };
+    return date.toLocaleTimeString("en-US", options);
   };
 
   const formatDate = (date) => {
-    const options = { year: 'numeric', month: 'long', day: '2-digit' };
-    return date.toLocaleDateString('en-US', options);
+    const options = { year: "numeric", month: "long", day: "2-digit" };
+    return date.toLocaleDateString("en-US", options);
   };
 
   const toggleFullscreen = () => {
     if (!document.fullscreenElement) {
       document.documentElement.requestFullscreen().catch((err) => {
-        console.log(`Error attempting to enable full-screen mode: ${err.message} (${err.name})`);
+        console.log(
+          `Error attempting to enable full-screen mode: ${err.message} (${err.name})`
+        );
       });
     } else {
       document.exitFullscreen();
@@ -37,18 +44,31 @@ const Navbar = ({ toggleSidebar, onLogout }) => {
 
   return (
     <nav className="navbar">
-      <span id='m1' className="material-symbols-outlined" onClick={toggleSidebar}>menu</span>
+      <span
+        id="m1"
+        className="material-symbols-outlined"
+        onClick={toggleSidebar}
+      >
+        menu
+      </span>
       <div className="logo">
-        <img id='img' src={logo} alt="Logo" />
+        <img id="img" src={logo} alt="Logo" />
       </div>
       <div className="dash">|</div>
       <div className="navbar-datetime">
-        <span>{formatDate(currentTime)} {formatTime(currentTime)}</span>
+        <span>
+          {formatDate(currentTime)} {formatTime(currentTime)}
+        </span>
       </div>
       <div className="dash">|</div>
       <div className="navbar-user">
-        <span>Mr. ABC</span><br></br>
-        <FontAwesomeIcon icon={faSignOutAlt} aria-label="Sign Out" onClick={onLogout} />
+        <span>Mr. ABC</span>
+        <br></br>
+        <FontAwesomeIcon
+          icon={faSignOutAlt}
+          aria-label="Sign Out"
+          onClick={onLogout}
+        />
       </div>
       <div className="dash">|</div>
       <div className="navbar-icons" onClick={toggleFullscreen}>
