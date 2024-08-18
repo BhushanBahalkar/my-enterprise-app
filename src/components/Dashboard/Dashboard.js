4cf80164cc3dcf1f74772aa6a44ce27596927bae
Bhushan Bahalkar <bhushanbahalkar189@gmail.com>
2024-08-18 15:04:37 +0530

    update Dashboard.js

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -17,6 +17,7 @@ import {
 } from "recharts";
 import "./Dashboard.css";
 const data = [
+
   { name: "Monday", uv: 4000, pv: 2400, amt: 2400 },
   { name: "Tuesday", uv: 3000, pv: 1398, amt: 2210 },
   { name: "Wednesday", uv: 2000, pv: 9800, amt: 2290 },
@@ -24,13 +25,16 @@ const data = [
   { name: "Friday", uv: 1890, pv: 4800, amt: 2181 },
   { name: "Saturday", uv: 2390, pv: 3800, amt: 2500 },
   { name: "Sunday", uv: 3490, pv: 4300, amt: 2100 },
+
 ];
 
 const pieData = [
+
   { name: "Group A", value: 400 },
   { name: "Group B", value: 300 },
   { name: "Group C", value: 300 },
   { name: "Group D", value: 200 },
+
 ];
 
 const COLORS = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042"];
